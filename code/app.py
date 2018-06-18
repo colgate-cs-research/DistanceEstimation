0c365c8390ed668141b4891822d0b4ed06934432
Yasoob Khalid <ykhalid@frappuccino.research.cs.colgate.edu>
2018-06-18 01:51:54 -0400

    changed the interface to ubuntu's

--- code/app.py
+++ code/app.py
@@ -1,6 +1,7 @@
+from __future__ import print_function
 from scapy.all import Dot11,Dot11Beacon,Dot11Elt,RadioTap,sendp,hexdump
 netSSID = 'testSSID' #Network name here
-iface = 'en0'   #Interface name here
+iface = 'wlp2s0'   #Interface name here
 
 dot11 = Dot11(type=0, subtype=8, addr1='ff:ff:ff:ff:ff:ff',
 addr2='22:22:22:22:22:22', addr3='33:33:33:33:33:33')
@@ -13,6 +14,8 @@ frame = RadioTap()/dot11/beacon/essid
 frame.show()
 print("\nHexdump of frame:")
 hexdump(frame)
-input("\nPress enter to start\n")
 
+
+#val = input("\nPress enter to start\n")
 sendp(frame, iface=iface, inter=0.100, loop=1)
+
